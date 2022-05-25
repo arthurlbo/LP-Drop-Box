bc8ecd317b04124ed0de266a53c4519e565f4532
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2022-05-25 17:55:28 -0300

    Add-Styles

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import GlobalStyles from "./Styles/GlobalStyles"
+
+function App() {
+
+  return (
+   <> 
+    <h1>Hello world!</h1>
+    
+
+    <GlobalStyles/>
+   </>
+  )
+}
+
+export default App
