106d37c580ff430e56220d5044cff8671b31f511
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2022-05-26 17:12:36 -0300

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
-import GlobalStyles from "./Styles/GlobalStyles"
+import React from 'react';
+import GlobalStyles from './Styles/GlobalStyles';
+
+import Data from './Data/Index';
+import Section from './Components/Section';
+import SideMenu from './Components/SideMenu';
+import MenuForm from './Components/MenuForm';
+
 
 function App() {
+    return (
+        <>
+            <Section
+                variant="blue"
+                title={Data[0].title}
+                description={Data[0].description}
+            />
+            <Section
+                variant="beige"
+                title={Data[1].title}
+                description={Data[1].description}
+            />
+            <Section
+                variant="blue"
+                title={Data[2].title}
+                description={Data[2].description}
+            />
+            <Section
+                variant="white"
+                title={Data[3].title}
+                description={Data[3].description}
+            />
+            <Section
+                variant="black"
+                title={Data[4].title}
+                description={Data[4].description}
+            />
 
-  return (
-   <> 
-    <h1>Hello world!</h1>
-    
+            <SideMenu>
+                <MenuForm />
+            </SideMenu>
 
-    <GlobalStyles/>
-   </>
-  )
+            <GlobalStyles />
+        </>
+    );
 }
 
-export default App
+export default App;
